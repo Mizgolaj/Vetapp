9d123196e0850ab2f22821c5ed1e957cb3be32de
Maciej Przezwicki <maciej.przezwicki@gmail.com>
2024-04-17 22:03:50 +0200

    commit 3: hibernate.properties file added, application.yml file added, Dog class annotation added

--- src/main/java/com/mizgmapr/project/classes/Main.java
+++ src/main/java/com/mizgmapr/project/classes/Main.java
@@ -1,7 +1,15 @@
 package com.mizgmapr.project.classes;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Main {
@@ -9,6 +17,8 @@ public class Main {
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 		System.out.println("Hello");
+
+
 
 		System.out.println("---------------Candidate Owner = Starting test -----------------");
 		CandidateOwner Maciek = new CandidateOwner("Maciek", "Kowalski", "ccc");
@@ -22,7 +32,13 @@ public class Main {
 		System.out.println(Maciek.hashCode());
 		System.out.println("---------------Candidate Owner = Testing over-----------------");
 
+		System.out.println("-------------table test--------------------");
 
-
+		System.out.println("-------------test over--------------------");
 	}
+
+	
+
+
+
 }
